891a0a08c50696670fb8234bcfc8df2539496302
VIRUZZ-OFC <113963745+VIRUZOFC@users.noreply.github.com>
2024-06-17 19:19:25 -0400

    Update fun-simi.js

--- plugins/fun-simi.js
+++ plugins/fun-simi.js
@@ -1,31 +1,74 @@
 import translate from '@vitalets/google-translate-api';
 import fetch from 'node-fetch';
 const handler = async (m, {text, command, args, usedPrefix}) => {
-if (!text) throw `${lenguajeGB['smsAvisoMG']()}𝙀𝙎𝘾𝙍𝙄𝘽𝘼 𝙐𝙉 𝙏𝙀𝙓𝙏𝙊 𝙋𝘼𝙍𝘼 𝙃𝘼𝘽𝙇𝘼𝙍 𝘾𝙊𝙉𝙈𝙄𝙂𝙊\n\n𝙀𝙅𝙀𝙈𝙋𝙇𝙊\n*${usedPrefix + command} Hola Gata Bot*\n\n𝙒𝙍𝙄𝙏𝙀 𝘼 𝙏𝙀𝙓𝙏 𝙏𝙊 𝙏𝘼𝙇𝙆 𝙏𝙊 𝙈𝙀\n\n𝙀𝙓𝘼𝙈𝙋𝙇𝙀\n*${usedPrefix + command} Hello Gata Bot*`
-try {
+  if (!text) {
+    conn.reply(
+      m.chat,
+      `Hola *@${m.sender.split`@`[0]}* Quieres charlar un rato? \nResponde con *${usedPrefix + command}* (tu mensaje) \n\n*_📌 Ejemplo :_* *${
+        usedPrefix + command
+      }* Hola bot`,
+      m,
+      {mentions: [m.sender]}
+    );
+    throw false;
+  }
+  try {
 await conn.sendPresenceUpdate('composing', m.chat)
-const api = await fetch('https://api.simsimi.net/v2/?text=' + text + '&lc=es');
-const resSimi = await api.json();
-m.reply(resSimi.success);
-} catch {
-try {
-if (text.includes('Hola')) text = text.replace('Hola', 'Hello');
-if (text.includes('hola')) text = text.replace('hola', 'Hello');
-if (text.includes('HOLA')) text = text.replace('HOLA', 'HELLO');
-const reis = await fetch('https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=en&dt=t&q=' + text);
-const resu = await reis.json();
-const nama = m.pushName || '1';
-const api = await fetch('http://api.brainshop.ai/get?bid=153868&key=rcKonOgrUFmn5usX&uid=' + nama + '&msg=' + resu[0][0][0]);
-const res = await api.json();
-const reis2 = await fetch('https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=es&dt=t&q=' + res.cnt);
-const resu2 = await reis2.json();
-m.reply(resu2[0][0][0]);
-} catch (e) {
-await m.reply(lenguajeGB['smsMalError3']() + '\n*' + lenguajeGB.smsMensError1() + '*\n*' + usedPrefix + `${lenguajeGB.lenguaje() == 'es' ? 'reporte' : 'report'}` + '* ' + `${lenguajeGB.smsMensError2()} ` + usedPrefix + command)
-console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
-console.log(e)
-}}}
-handler.help = ['simsimi']
-handler.tags = ['General']
-handler.command = ['bot', 'simi', 'simsimi'] 
-export default handler
+let gpt = await fetch(`https://delirius-api-oficial.vercel.app/api/simi?text=${text}`)
+let res = await gpt.json()
+await m.reply(res.data.message)
+  } catch {
+    try {
+      if (text.includes('Hola')) text = text.replace('Hola', 'Hello');
+      if (text.includes('hola')) text = text.replace('hola', 'Hello');
+      if (text.includes('HOLA')) text = text.replace('HOLA', 'HELLO');
+      const reis = await fetch('https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=en&dt=t&q=' + text);
+      const resu = await reis.json();
+      const nama = m.pushName || '1';
+      const api = await fetch('http://api.brainshop.ai/get?bid=153868&key=rcKonOgrUFmn5usX&uid=' + nama + '&msg=' + resu[0][0][0]);
+      const res = await api.json();
+      const reis2 = await fetch('https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=es&dt=t&q=' + res.cnt);
+      const resu2 = await reis2.json();
+      m.reply(resu2[0][0][0]);
+    } catch {
+      throw `Simsimi esta durmiendo no molesta 🥱`;
+    }
+  }
+};
+handler.help = ['simi']
+handler.tags = ['fun'];
+handler.command = /^((sim)?simi|alexa|cortana|bot)$/i;
+export default handler;
+
+
+//import translate from '@vitalets/google-translate-api';
+//import fetch from 'node-fetch'
+//const handler = async (m, {text, command, args, usedPrefix}) => {
+//if (!text) throw `${lenguajeGB['smsAvisoMG']()}𝙀𝙎𝘾𝙍𝙄𝘽𝘼 𝙐𝙉 𝙏𝙀𝙓𝙏𝙊 𝙋𝘼𝙍𝘼 𝙃𝘼𝘽𝙇𝘼𝙍 𝘾𝙊𝙉𝙈𝙄𝙂𝙊\n\n𝙀𝙅𝙀𝙈𝙋𝙇𝙊\n*${usedPrefix + command} Hola Gata Bot*\n\n𝙒𝙍𝙄𝙏𝙀 𝘼 𝙏𝙀𝙓𝙏 𝙏𝙊 𝙏𝘼𝙇𝙆 𝙏𝙊 𝙈𝙀\n\n𝙀𝙓𝘼𝙈𝙋𝙇𝙀\n*${usedPrefix + command} Hello Gata Bot*`
+//try {
+//await conn.sendPresenceUpdate('composing', m.chat)
+//  const api = await fetch('https://api.simsimi.net/v2/?text=' + text + '&lc=es');
+//const resSimi = await api.json();
+//m.reply(resSimi.success);
+//} catch {
+//try {
+//if (text.includes('Hola')) text = text.replace('Hola', 'Hello');
+//if (text.includes('hola')) text = text.replace('hola', 'Hello');
+//if (text.includes('HOLA')) text = text.replace('HOLA', 'HELLO');
+//const reis = await fetch('https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=en&dt=t&q=' + text);
+////const resu = await reis.json();
+//const nama = m.pushName || '1';
+//const api = await fetch('http://api.brainshop.ai/get?bid=153868&key=rcKonOgrUFmn5usX&uid=' + nama + '&msg=' + resu[0][0][0]);
+//const res = await api.json();
+//const reis2 = await fetch('https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=es&dt=t&q=' + res.cnt);
+//const resu2 = await reis2.json();
+//m.reply(resu2[0][0][0]);
+//} catch (e) {
+//await m.reply(lenguajeGB['smsMalError3']() + '\n*' + lenguajeGB.smsMensError1() + '*\n*' + usedPrefix + `${lenguajeGB.lenguaje() == 'es' ? 'reporte' : 'report'}` + '* ' + `${lenguajeGB.smsMensError2()} ` + usedPrefix + command)
+//console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
+//console.log(e)
+//}}}
+//handler.help = ['simsimi']
+//handler.tags = ['General']
+///handler.command = ['bot', 'simi', 'simsimi'] 
+//export default handler
